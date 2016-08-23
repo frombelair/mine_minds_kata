4f8cdffb6771ac9a1794bfdf9fd2abac121c121f
frombelair <lbenchouk@gmail.com>
2016-08-23 13:43:37 -0400

    changed

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -1,10 +1,12 @@
 number = 1
 100 . times do
-	if number % 3 == 0
-		puts "mined"
-		elsif number % 5 == 0
+	if number % 15 == 0
+		puts "minedminds"
+		elsif number %3 == 0
+			puts "mined"
+		elsif % 5 == 0
 			puts "minds"
-		else
+     else
 			puts number
 		end
 		number = number + 1
