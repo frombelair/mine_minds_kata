6c6de14ecdab8329f728b5665e5dd004cd7da417
frombelair <lbenchouk@gmail.com>
2016-08-28 16:19:08 -0400

    code 1..100

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -1,13 +1,20 @@
-number = 1
-100 . times do
-	if number % 15 == 0
-		puts "minedminds"
-		elsif number %3 == 0
-			puts "mined"
-		elsif % 5 == 0
-			puts "minds"
-     else
-			puts number
-		end
-		number = number + 1
-	end
+# number = 1
+# 100 . times do
+# 	if number % 15 == 0
+# 		puts "minedminds"
+# 		elsif number %3 == 0
+# 			puts "mined"
+# 		elsif % 5 == 0
+# 			puts "minds"
+#      else
+# 			puts number
+# 		end
+# 		number = number + 1
+# 	end
+
+
+number = 1 #sets variable for word number = 1
+100.times do
+	puts number
+	number = number + 1
+end
