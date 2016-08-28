b8f1038892fde2426401625b2b38af981deee99d
frombelair <lbenchouk@gmail.com>
2016-08-28 16:51:33 -0400

    type mined instead of 3 type minds instead of 5

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -17,8 +17,12 @@ number = 1 #sets variable for word number = 1
 100.times do
 	if number == 3 
 		puts "mined"
+	elsif 
+		number == 5 
+		puts "minds" 
 	else
 		puts number		
 	end	
 	number = number + 1
 end
+
