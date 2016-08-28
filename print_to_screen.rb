daf2118bfd432dec58920fa70eebea00388e0c82
frombelair <lbenchouk@gmail.com>
2016-08-28 16:58:09 -0400

    divisible numbers by 3 and 5 with 0 left write mined and minds

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -15,10 +15,10 @@
 
 number = 1 #sets variable for word number = 1
 100.times do
-	if number == 3 
+	if number % 3 == 0 
 		puts "mined"
 	elsif 
-		number == 5 
+		number % 5 == 0 
 		puts "minds" 
 	else
 		puts number		
