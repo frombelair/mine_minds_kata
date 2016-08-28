d8fc2af4bf488c5d27355dab97d014243cafb0c0
frombelair <lbenchouk@gmail.com>
2016-08-28 17:06:54 -0400

    if number divisible 3 and 5 type minedminds

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -15,7 +15,10 @@
 
 number = 1 #sets variable for word number = 1
 100.times do
-	if number % 3 == 0 
+	if number % 3 == 0 && number % 5 == 0
+		puts "minedminds"
+	elsif 
+		number % 3 == 0 
 		puts "mined"
 	elsif 
 		number % 5 == 0 
